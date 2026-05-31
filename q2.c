d619f0741f2520b41999cc0032e98631f0158f7d
Khushboo Chauhan <chauhankhushboo85@gmail.com>
2026-05-31 21:27:48 +0530

    coding assignment uploaded

---
+++ q2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+   printf("enter the no whose multiplication table we want to print: ");
+   scanf("%d", &n);
+   for(int i=1;i<=10;i++)
+{
+    printf("%d x %d = %d\n", n, i, n*i);
+} 
+ return 0;
+} 
---
+++ q3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {int n;
+  int fact=1;
+  printf("enter the number to find its factorial: ");
+  scanf("%d", &n);
+    for(int i=1;i<=n;i++)
+   {
+       fact= fact*i;
+    } 
+    printf("the factorial of the number is: %d\n", fact); 
+return 0;
+}
